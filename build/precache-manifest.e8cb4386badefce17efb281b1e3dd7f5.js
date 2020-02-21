d4f87783b4eb7a0221ad8c2e7e21e8a56f275e26
Douglas Thomaz <douglasthmz@hotmail.com>
2020-02-21 20:40:14 -0300

    first deploy

---
+++ build/precache-manifest.e8cb4386badefce17efb281b1e3dd7f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd803e4fd8b1db0301f98e970de8158",
+    "url": "/RepoShow/index.html"
+  },
+  {
+    "revision": "8a2937977f25019c445c",
+    "url": "/RepoShow/static/js/2.97590062.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/RepoShow/static/js/2.97590062.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ddff4ab06244bb64d4",
+    "url": "/RepoShow/static/js/main.f15a7f7e.chunk.js"
+  },
+  {
+    "revision": "5f7167cb25e2bd818d4f",
+    "url": "/RepoShow/static/js/runtime-main.7ff41e22.js"
+  }
+]);
